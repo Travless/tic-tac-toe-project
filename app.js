34e676cf0090bd27f2b94a2698b470f89afa10e9
Travless <travis.white0101@gmail.com>
2022-11-04 13:59:02 -0400

    begin to condense and organize code for game logic to fit within module, and sub sections within their own modules or factory functions.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-let gameArray = ['', '', '', '', '', '', '', '', ''];
-gameArray.length = 9;
-let playerOne = '';
-let compPlayer = '';
-let spaces = document.getElementById('gameboard-grid-container');
+//generates game via a module
+const genGame = (() => {
 
-const genBoard = (() => {
+    let gameArray = ['', '', '', '', '', '', '', '', ''];
+    gameArray.length = 9;
+    let playerOne = '';
+    let compPlayer = '';
+    let spaces = document.getElementById('gameboard-grid-container');
 
-    const gameFlow = () => {
-        // Generates if player will be X or O for their player pawn (will improve logic)
+    // Generates if player will be X or O for their player pawn (will improve logic)
+    const userPawns = () => {
         let playerPawn = prompt('X or O?');
         playerOne = playerPawn.toUpperCase();
         
@@ -19,108 +20,73 @@ const genBoard = (() => {
         return { playerOne, compPlayer };
     };
 
-    gameFlow();
+    userPawns();
 
-    const gameBoard = document.getElementById('gameboard-grid-container');
-    let gameCol1 = document.createElement('div');
-    gameCol1.classList.add('space-container');
-    gameBoard.append(gameCol1);
-    let gameCol2 = document.createElement('div');
-    gameCol2.classList.add('space-container');
-    gameBoard.append(gameCol2);
-    let gameCol3 = document.createElement('div');
-    gameCol3.classList.add('space-container');
-    gameBoard.append(gameCol3);
+    //generates gameboard at beginning of the game, after the player chooses their pawn
+    const genGameboard = (() => {
+        const gameBoard = document.getElementById('gameboard-grid-container');
+        let gameCol1 = document.createElement('div');
+        gameCol1.classList.add('space-container');
+        gameBoard.append(gameCol1);
+        let gameCol2 = document.createElement('div');
+        gameCol2.classList.add('space-container');
+        gameBoard.append(gameCol2);
+        let gameCol3 = document.createElement('div');
+        gameCol3.classList.add('space-container');
+        gameBoard.append(gameCol3);
 
-    for(let i = 0; i < 3; i++){
-        let space = document.createElement('div');
-        space.classList.add('space');
-        gameCol1.append(space);
-        space.setAttribute('space-number', i);
-    }
-    for(let i = 3; i < 6; i++){
-        let space = document.createElement('div');
-        space.classList.add('space');
-        gameCol2.append(space);
-        space.setAttribute('space-number', i);
-    }
-    for(let i = 6; i < 9; i++){
-        let space = document.createElement('div');
-        space.classList.add('space');
-        gameCol3.append(space);
-        space.setAttribute('space-number', i);
-    }
-})();
+        for(let i = 0; i < 3; i++){
+            let space = document.createElement('div');
+            space.classList.add('space');
+            gameCol1.append(space);
+            space.setAttribute('space-number', i);
+        }
+        for(let i = 3; i < 6; i++){
+            let space = document.createElement('div');
+            space.classList.add('space');
+            gameCol2.append(space);
+            space.setAttribute('space-number', i);
+        }
+        for(let i = 6; i < 9; i++){
+            let space = document.createElement('div');
+            space.classList.add('space');
+            gameCol3.append(space);
+            space.setAttribute('space-number', i);
+        }
+    })();
+    console.log(gameArray);
 
-console.log(gameArray);
+    //executes a single move for both the human player and the AI when the player clicks on one of the game spaces
+    let gameMoves = (() => {
+        spaces.addEventListener('click', function(event){
+            let moveLocation = event.target.getAttribute('space-number');
+            // console.log(moveLocation);
+            if(gameArray[moveLocation] === 'X' || gameArray[moveLocation] === 'O'){
+                alert('spot taken, pick again');
+                return;
+            } else {
+                gameArray[moveLocation] = playerOne;
+                let playerSymbolDisplay = event.target;
+                playerSymbolDisplay.textContent = playerOne;
+            };
 
-//executes a single move for both the human player and the AI when the player clicks on one of the game spaces
-let playerOneMove = () => {
-    spaces.addEventListener('click', function(event){
-        let moveLocation = event.target.getAttribute('space-number');
-        // console.log(moveLocation);
-        if(gameArray[moveLocation] === 'X' || gameArray[moveLocation] === 'O'){
-            alert('spot taken, pick again');
-            return;
-        } else {
-            gameArray[moveLocation] = playerOne;
-            let playerSymbolDisplay = event.target;
-            playerSymbolDisplay.textContent = playerOne;
-        };
+            let randomIndex = Math.floor(Math.random() * 9);
+            while(gameArray[randomIndex] === 'X' || gameArray[randomIndex] === 'O'){
+                if(gameArray.includes('')){
+                    randomIndex = Math.floor(Math.random() * 9);
+                } else {
+                    return;
+                }
+            };
+            gameArray[randomIndex] = compPlayer;
+            let compSymbolDisplay = document.querySelector(`[space-number="${randomIndex}"]`);
+            compSymbolDisplay.textContent = compPlayer;
+            // console.log(gameArray);
+            // console.log(randomIndex);
 
-        let randomIndex = Math.floor(Math.random() * 9);
-        while(gameArray[randomIndex] === 'X' || gameArray[randomIndex] === 'O'){
-            randomIndex = Math.floor(Math.random() * 9);
-        };
-        gameArray[randomIndex] = compPlayer;
-        let compSymbolDisplay = document.querySelector(`[space-number="${randomIndex}"]`);
-        compSymbolDisplay.textContent = compPlayer;
-        console.log(gameArray);
-        console.log(randomIndex);
+            //logs the elements within the desired indexes of gameArray
+            console.log([0,1,2].map(x=>gameArray[x]));
         
-    });
-};
-
-playerOneMove();
-// compMove();
-
-
-// const gameBoard = (() => {
-//     let gameArray = ['', '', '', '', '', '', '', '', ''];
-//     return { gameArray };
-// })();
-
-// const player = (pawn) => {
-//     const getPawn = () => pawn;
-//     return { getPawn };
-// };
-
-// const gameFlow = (() => {
-//     // Generates if player will be X or O for their player pawn (will improve logic)
-//     let playerPawn = prompt('X or O?');
-//     const playerOne = player(playerPawn.toUpperCase());
-
-//     if (playerPawn === 'X') {
-//         let compPawn = 'O';
-//         return compPawn;
-//     } else if (playerPawn === 'O') {
-//         let compPawn = 'X';
-//         return compPawn;
-//     }
-// }();
-
-// const genBoard = (() => {
-//     let gameArray = ['', '', '', '', '', '', '', '', ''];
-//     const gameBoard = document.getElementById('gameboard-grid-container');
-//     for(let i = 0; i < 3; i++){
-//         let gameSpaceCol = document.createElement('div');
-//         gameSpaceCol.classList.add('space-col');
-//         gameBoard.append(gameSpaceCol);
-//         for (let j = 0; j < 3; j++){
-//             let gameSpace = document.createElement('div');
-//             gameSpace.classList.add('space');
-//             gameSpaceCol.append(gameSpace)
-//         }
-//     }
-//     return { gameArray };
-// })();
+        });
+    })();
+})();
